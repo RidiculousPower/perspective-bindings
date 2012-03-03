b90d5240ac8618c55414a6bf2f022688f592e1e0
Asher <asher@ridiculouspower.com>
2012-03-03 05:41:17 -0500

    Revert "Revert "mostly spec'd; commiting before removing #ensure_configured_for_html_node""
    This reverts commit 86aa6e8afa44ea3abdfe67a53bf51c79f34510bf.

--- lib/rmagnets-bindings/Rmagnets/Bindings/ObjectInstance.rb
+++ lib/rmagnets-bindings/Rmagnets/Bindings/ObjectInstance.rb
@@ -1,18 +1,29 @@
 
-#-----------------------------------------------------------------------------------------------------------#
-#----------------------------------  Rmagnets Bindings - Object Instance  ----------------------------------#
-#-----------------------------------------------------------------------------------------------------------#
-
-module Rmagnets::Bindings::ObjectInstance
+module ::Rmagnets::Bindings::ObjectInstance
 	
+  include ::CascadingConfiguration::Array
+  include ::CascadingConfiguration::Hash
+
+	attr_configuration_hash          :attribute_bindings, :required_bindings_hash, 
+	                                 :attribute_rebindings, :binding_aliases
+  attr_configuration_array         :binding_order
+
+  ################
+  #  initialize  #
+  ################
+
+  def initialize
+    
+    @bound_bindings = { }
+		
+  end
+  
   ##########
   #  bind  #
   ##########
 
 	def bind( *object_binding )
 		
-		@bound_bindings ||= Hash.new
-		 
 		until object_binding.empty?
 			
 			this_object_binding = object_binding.shift
@@ -20,15 +31,22 @@ module Rmagnets::Bindings::ObjectInstance
 			if this_object_binding.is_a?( Hash )
 				
 				this_object_binding.each do |this_binding, this_object|
-					raise 'No binding defined for :' + this_binding.to_s + ' in class ' + self.class.to_s + '.' unless self.class.has_binding?( this_binding )
-					@bound_bindings[ this_binding ] = this_object
+
+				  bind( this_binding, this_object )
+
 				end
 				
 			else
 				
 				this_binding 	= this_object_binding
 				this_object		=	object_binding.shift
-				raise 'No binding defined for :' + this_binding.to_s + ' in class ' + self.class.to_s + '.' unless self.class.has_binding?( this_binding )
+				
+				unless self.class.has_binding?( this_binding )
+      		raise ::Rmagnets::Bindings::Exception::NoBindingError,
+      		      'No binding defined for :' + binding_name.to_s + ' in class ' + 
+				        self.class.to_s + '.'
+				end
+				
 				@bound_bindings[ this_binding ] = this_object
 				
 			end
@@ -39,26 +57,50 @@ module Rmagnets::Bindings::ObjectInstance
 		
 	end
 
+  ############
+  #  unbind  #
+  ############
+
+	def unbind( *object_bindings )
+		
+		object_bindings.each do |this_binding|
+      
+      @bound_bindings.delete( this_binding )
+      
+    end
+    
+  end
+  
   #############
   #  binding  #
   #############
 
 	def binding( *binding_names )
 		
-		@bound_bindings ||= Hash.new
-		
 		binding_instance = nil
+
 		until binding_names.empty?
+
 			this_binding_name = binding_names.shift
+			
 			if binding_instance
+				
 				binding_instance = binding_instance.binding( this_binding_name )
+			
 			else
+				
 				if self.class.binding_is_rebinding?( this_binding_name )
 					this_binding_name = self.class.binding_for_rebinding( this_binding_name )
 				end
-				raise 'No binding defined for :' + this_binding_name.to_s + ' in ' + self.class.to_s + '.' unless self.class.has_binding?( this_binding_name )
+				
+				unless self.class.has_binding?( this_binding_name )
+				  raise 'No binding defined for :' + this_binding_name.to_s + ' in ' + self.class.to_s + '.'
+				end
+				
 				binding_instance = @bound_bindings[ this_binding_name ]
+			
 			end
+
 		end
 
 		return binding_instance
@@ -71,21 +113,18 @@ module Rmagnets::Bindings::ObjectInstance
 
 	def has_binding?( binding )
 		
-		@bound_bindings ||= Hash.new
-		
-		if self.class.binding_is_rebinding?( binding )
-			binding = self.class.binding_for_rebinding( binding )
-		end
-		
 		return @bound_bindings.has_key?( binding )
+
 	end
 
   ###############################################
-  #  binding_requires_rendering_to_xhtml_node?  #
+  #  binding_requires_rendering_to_html_node?  #
   ###############################################
 	
-	def binding_requires_rendering_to_xhtml_node?( binding )
-		return binding( binding ).respond_to?( :to_xhtml_node )
+	def binding_requires_rendering_to_html_node?( binding )
+	  
+		return binding( binding ).respond_to?( :to_html_node )
+
 	end
 	
 	###########################
@@ -105,28 +144,40 @@ module Rmagnets::Bindings::ObjectInstance
 	
 		# cascade through bindings and objects until we find one that has the requested binding
 		cascading_bindings_or_objects.each do |this_binding_or_object|
+
 			if this_binding_or_object.is_a?( Symbol )
+
 				if binding( this_binding_or_object ).has_binding?( binding_name )
 					binding_instance = binding( this_binding_or_object ).binding( binding_name )
 					break
 				end
+
 			else
+
 				if this_binding_or_object.has_binding?( binding_name )
 					binding_instance = this_binding_or_object.binding( binding_name )
 					break
 				end
+
 			end
+
 		end
 		
 		# if we don't have a binding instance from bindings or objects, use default
 		unless binding_instance
+
 			# default can be a class, in which case we instantiate it
 			if default_view_or_class.is_a?( Class )
+
 				binding_instance = default_view_or_class.new
+
 			# or it can be an instance, in which case we simply bind it
 			else
+
 				binding_instance = default_view_or_class
+
 			end
+
 		end
 	
 		# finally, bind our requested instance
@@ -135,16 +186,32 @@ module Rmagnets::Bindings::ObjectInstance
 		return self
 		
 	end
-	
-	###################
-	#  share_binding  #
-	###################
-
-	def share_binding( binding_name, with_binding_name )
 		
+	#####################
+	#  cascade_binding  #
+	#####################
+
+  # cascade_binding( :binding_name_in_self, :to_same_binding_name_in_binding )
+  # cascade_binding( :binding_name_in_self, :in_binding => :to_other_binding_name )
+	def cascade_binding( binding_name, cascade_descriptor_hash_or_binding )
+		
+		binding_cascade_fills = nil
+		
+		if cascade_descriptor_hash_or_binding.is_a?( Hash )
+
+  		binding_cascade_fills = nil
+		  
+	  else
+	    
+	    binding_cascade_fills = binding_name
+  		
+    end
+				
 		# link binding in self with binding in with_binding_name unless with_binding_name already has a binding
 		unless binding( with_binding_name ).has_binding?( binding_name )
+		  
 			binding( with_binding_name ).bind( binding_name => binding( binding_name ) ) 
+		
 		end
 		
 		return self
