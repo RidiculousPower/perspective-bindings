7b5555bc2adf40747efdbddaa684cc3c49875c7e
Asher Haig <asher@ridiculouspower.com>
2013-02-27 12:55:51 -0500

    updated description for number binding definition

--- spec/perspective/bindings/binding_definitions/number.rb
+++ spec/perspective/bindings/binding_definitions/number.rb
@@ -8,7 +8,7 @@ shared_examples_for :number_container_binding do
   ##############################
 
   context '#__binding_value_valid__?' do
-    it( 'will match :number' ) { should match_types( :integer, :float, :rational, :complex ) }
+    it( 'will match :integer, :float, :rational, :complex' ) { should match_types( :integer, :float, :rational, :complex ) }
   end
   
 end
